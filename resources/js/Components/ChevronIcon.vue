aa3ebcff07c180dc3a56049b40a8fc09f9d8dab2
yognevoy <ya.ognevoy@gmail.com>
2026-08-28 11:10:38 +0300

    refactor: delete unused resume template components

--- resources/js/Components/ChevronIcon.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <svg class="h-3.5 w-3.5" viewBox="0 0 16 16" fill="none" stroke="currentColor" stroke-width="1.5">
-        <path d="M10 3l-5 5 5 5" stroke-linecap="round" stroke-linejoin="round" />
-    </svg>
-</template>
